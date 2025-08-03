160bc86342c2f2706c8d9f60b485ec216fca3035
Gregoris Bachtsevanos <94171068+gregorisbachtsevanos@users.noreply.github.com>
2025-08-03 15:28:50 +0300

    Update aggregator.py

--- aggregator.py
+++ aggregator.py
@@ -1,9 +1,13 @@
-from sources import mock_source
+from sources.eventbrite_source import search_events as eb_search
+from sources.predicthq_source import search_events as phq_search
 from models import Event
 from typing import List
 
-def get_all_events() -> List[Event]:
+def get_all_events(
+    lat: float, lon: float, radius_km: float,
+    start: str=None, end: str=None, phq_categories=None
+) -> List[Event]:
     events = []
-    events.extend(mock_source.get_mock_events())
-    # more sources should added here in future
+    events += eb_search(lat, lon, f"{radius_km}km", start, end)
+    events += phq_search(lat, lon, radius_km, categories=phq_categories, start=start, end=end)
     return events
